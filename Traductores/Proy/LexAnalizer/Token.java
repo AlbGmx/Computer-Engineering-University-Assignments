596db37ff2c196386cd329e302779db080f9cc34
Emmanuel <Gomez.emmanuel@uabc.edu.mx>
2022-10-26 16:39:35 -0700

    trad proy

---
+++ Traductores/Proy/LexAnalizer/Token.java
@@ -0,0 +1,14 @@
+package Proy.LexAnalizer;
+
+public class Token {
+   public int type;
+   public String text;
+   public Token(int type, String string){
+      this.type=type;
+      this.text=string;
+   }
+   public String toString(){
+      String tname = Listlexer.tokenNames[type];
+      return "<'"+text+"',"+tname+">";
+   }
+}
