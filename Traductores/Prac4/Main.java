5137ced5959e1aa8dc9cb5e8282a28513027d25e
Emmanuel Cardenas <emmanuel.cardenas@dexcom.com>
2022-11-29 00:45:38 -0800

    safety backup

--- Traductores/Prac4/Main.java
+++ Traductores/Prac4/Main.java
@@ -1,11 +1,8 @@
 public class Main { 
 
     public static void main(String args[]) {
-
-        String test = "[a, ]";
-
-        //ListLexer lexer = new ListLexer(args[0]);
-        ListLexer lexer = new ListLexer(test);
+      //ListLexer lexer = new ListLexer(args[0]);
+        ListLexer lexer = new ListLexer("[a]");
         ListParser parser = new ListParser(lexer);
         parser.list();
         System.out.println("Correct input, no errors found.");
