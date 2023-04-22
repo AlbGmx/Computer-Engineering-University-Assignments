7a6ff78a9e95a73bff85e80bf77b792b73a1b21e
Emmanuel Alberto Gomez Cardenas <gomez.emmanuel@uabc.edu.mx>
2023-04-21 21:46:37 -0700

    Microcontroladores: Taller 7a, Uso de Timers

--- AsignaturasActuales/Micros/Talleres/T7/timer.c
+++ AsignaturasActuales/Micros/Talleres/T7/timer.c
@@ -1,75 +1,70 @@
 /*
  * Autor: Emmanuel Alberto Gómez Cárdenas
  * Fecha: 21/04/2023
- * Descripcion: Taller 7a - TIMG
+ * Descripcion: Taller 7a, Uso de timers en ESP32 
+ *    mediante registros sin el uso de interrupciones
  * Taller de la clase Microcontroladores
- */
+ */ 
+
+#include <stdio.h>
+#include <time.h>  //To test the Delay25mS() function
 
 // Define the timer registers addresses
-#define TIMG0_T0CONFIG_REG *(uint32_t *)0x3FF5F000
-#define TIMG0_T0LO_REG *(uint32_t *)0x3FF5F004
-#define TIMG0_T0HI_REG *(uint32_t *)0x3FF5F008
-#define TIMG0_T0UPDATE_REG *(uint32_t *)0x3FF5F00C
-#define TIMG0_T0ALARMLO_REG *(uint32_t *)0x3FF5F010
-#define TIMG0_T0ALARMHI_REG *(uint32_t *)0x3FF5F014
-#define TIMG0_T0LOADLO_REG *(uint32_t *)0x3FF5F018
-#define TIMG0_T0LOADHI_REG *(uint32_t *)0x3FF5F01C
-#define TIMG0_T0LOAD_REG *(uint32_t *)0x3FF5F020
+#define TIMG0_T0CONFIG_REG 0x3FF5F000
+#define TIMG0_T0LO_REG     0x3FF5F004
+#define TIMG0_T0HI_REG     0x3FF5F008
+#define TIMG0_T0UPDATE_REG 0x3FF5F00C
+#define TIMG0_T0LOADLO_REG 0x3FF5F018
+#define TIMG0_T0LOADHI_REG 0x3FF5F01C
+#define TIMG0_T0LOAD_REG   0x3FF5F020
+#define TESTRUNS               100
 
-void setup() {
-   // Show current config after boot
-   printCurrentTIMG0_T0_Values();
+void Delay25mS() {
+   // Define all registers as uint32_t pointers
+   uint32_t *configurationRegister =    (uint32_t *) TIMG0_T0CONFIG_REG;
+   uint32_t *currentLowValueRegister =  (uint32_t *) TIMG0_T0LO_REG;
+   uint32_t *currentHighValueRegister = (uint32_t *) TIMG0_T0HI_REG;
+   uint32_t *loadLowToRegister =        (uint32_t *) TIMG0_T0LOADLO_REG;
+   uint32_t *loadHighToRegister =       (uint32_t *) TIMG0_T0LOADHI_REG;
+   uint32_t *loadRegisters =            (uint32_t *) TIMG0_T0LOAD_REG;
+   uint32_t *updateValueRegister =      (uint32_t *) TIMG0_T0UPDATE_REG;
 
-   // Set prescaler to divide clock by 80,000,000
-   TIMG0_T0CONFIG_REG |= (79999999 << 12);
+   // Disable the timer (31th bit) in case it was activated
+   *configurationRegister &= ~(1 << 30);
 
-   // Set reload value to 1
-   TIMG0_T0LOAD_REG = 1;
+   // Set prescaler (13th to 28th bits) to 80,000 to count each ms
+   *configurationRegister |= (80000 << 12);
 
-   // Clear autoreload bit
-   TIMG0_T0CONFIG_REG &= ~(1 << 29);
+   // Clear autoreload (29th) bit
+   *configurationRegister &= ~(1 << 29);
 
-   // Clear current timer
-   TIMG0_T0LOADLO_REG = 0;
-   TIMG0_T0LOADHI_REG = 0;
-   TIMG0_T0LOAD_REG = 0;
+   // Clear current values in timer
+   *loadLowToRegister = 0;
+   *loadHighToRegister = 0;
+   *loadRegisters = 0;
+   *updateValueRegister = 1;
 
-   // Show current config after configuration
-   printCurrentTIMG0_T0_Values();
+   // Enable the timer (31st bit)
+   *configurationRegister |= (1 << 30);
 
-   // Enable the timer
-   TIMG0_T0CONFIG_REG |= (1 << 31);
-   printf("\n\nStart counting\n");
-   Delay();
-}
-
-/* Función de retardo 25ms */
-void loop() {
-}
-
-void Delay() {
-   while (true) {
-      TIMG0_T0UPDATE_REG = 0;
-      printf("Actual TIMG0 0x%08x %08x\n", TIMG0_T0HI_REG, TIMG0_T0LO_REG);
-
-      // when alarm is triggered exit function
-      if (TIMG0_T0HI_REG) {
-         printf("\nEND counting\n");
-         printCurrentTIMG0_T0_Values();
-         return;
-      }
+   // Actual Delay
+   while (*currentLowValueRegister < 25) {
+      *updateValueRegister = 1;
+      printf(" ");  // TO DO, function breaks when this line is removed
    }
-}
 
-void printCurrentTIMG0_T0_Values() {
-   TIMG0_T0UPDATE_REG = 0;
-   printf("\n\nActual TIMG0 config, 0x%08x\n", TIMG0_T0CONFIG_REG);
-   printf("Actual TIMG0 LO, 0x%08x\n", TIMG0_T0LO_REG);
-   printf("Actual TIMG0 HI, 0x%08x\n", TIMG0_T0HI_REG);
-   printf("Actual TIMG0 UPDATE, 0x%08x\n", TIMG0_T0UPDATE_REG);
-   printf("Actual TIMG0 ALARMLO, 0x%08x\n", TIMG0_T0ALARMLO_REG);
-   printf("Actual TIMG0 ALARMHI, 0x%08x\n", TIMG0_T0ALARMHI_REG);
-   printf("Actual TIMG0 LOADLO, 0x%08x\n", TIMG0_T0LOADLO_REG);
-   printf("Actual TIMG0 LOADLO, 0x%08x\n", TIMG0_T0LOADHI_REG);
-   printf("Actual TIMG0 LOAD, 0x%08x\n\n", TIMG0_T0LOAD_REG);
+   // Disable the timer (31st bit)
+   *configurationRegister &= ~(1 << 30);
+}
+void app_main() {
+   clock_t start;
+   double sum = 0;
+   for (int i = 0; i < TESTRUNS; i++) {
+      start = clock();  // Record the start time
+      Delay25mS();      // Call the function to be timed
+      sum += (double)(clock() - start) / CLOCKS_PER_SEC;
+      printf("The time of run %d is %f seconds\n", i, (double)(clock() - start) / CLOCKS_PER_SEC);
+   }
+
+   printf("The average time is %f seconds\n", sum / TESTRUNS);
 }
